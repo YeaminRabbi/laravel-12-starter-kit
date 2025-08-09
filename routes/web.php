f88a923befd3d35fbdbf27468ec28a50785628c3
yeaminrabbi <yeaminrabbi308@gmail.com>
2025-08-09 14:07:06 +0600

    installed tailwind & livewire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\HomeLivewire;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeLivewire::class)->name('home');
