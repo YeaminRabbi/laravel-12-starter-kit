81ef6f62992c4e4e44c1ccfe02d40eee46e29f00
Yeamin Rabbi <44961810+YeaminRabbi@users.noreply.github.com>
2025-10-25 12:18:19 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,12 @@
 
 use App\Livewire\HomeLivewire;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', HomeLivewire::class)->name('home');
+
+Route::get('/foo', function () {
+    rescue(fn () => Artisan::call('storage:link'), null, report: false);
+
+    return to_route('home');
+});
