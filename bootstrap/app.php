e9373522bc686aca868fbf9498efe2daddf4125c
yeaminrabbi <yeaminrabbi308@gmail.com>
2025-12-17 13:05:49 +0600

    added cors - Laravel + Custom

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\RemovePublicSegment;
+use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Http\Request;
+use App\Http\Middleware\Cors;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +17,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Custom CORS Middleware
+        $middleware->append(Cors::class);
+
+        // Laravel CORS Middleware
+        // $middleware->append(HandleCors::class);
+
+
         $middleware->prependToGroup('web', RemovePublicSegment::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+         $exceptions->render(function (Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['status' => false, 'message' => 'Data not found.'], 404);
+            }
+        });
     })->create();
